6d60ef7d32fe7a85fd34d5c768b74ab8657387a2
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-12-27 06:27:04 +0700

    Lab3 - Bai 2

---
+++ Lesson3/Lab3_Bai2.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class Lab3_Bai2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhập số nguyên
+        System.out.print(" Nhập số nguyên: ");
+        int x = scanner.nextInt();
+
+        // In bảng cửu chương
+        System.out.println("Bảng cửu chương của " + x + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", x, i, x * i);
+        }
+        scanner.close();
+    }
+
+}
