cae0907807e44bd6db47643188f07e3db2d7763f
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-12-26 11:45:29 +0700

    Method Overloangding - Scope

---
+++ Pratices/Session5.java
@@ -0,0 +1,10 @@
+package Pratices;
+
+// Method Scope
+// Java Scope, In Java,variables are only accessible inside the region they are created.This is called scope.
+public class Session5 {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(x);
+    }
+}
