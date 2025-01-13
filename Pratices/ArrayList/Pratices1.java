fd31ec9be7b4e79b5083581108571b444174a47e
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2025-01-13 13:30:49 +0700

    Array List

---
+++ Pratices/ArrayList/Pratices1.java
@@ -0,0 +1,13 @@
+package Pratices.ArrayList;
+
+import java.util.ArrayList; // import the ArrayList class
+
+public class Pratices1 {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
